a0d533723a0ab9a4b02b782de121427522846270
Pedro Braga Miranda <pbragamiranda@pm.me>
2024-02-11 20:22:47 +0100

    adding testing gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require 'vcr'
 require 'rack/test'
+require 'webmock/rspec'
 require_relative '../models/story'
 require_relative '../models/character'
 require_relative '../services/marvel_api_service'
@@ -21,4 +23,10 @@ end
 
 def app
   Sinatra::Application
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
 end
