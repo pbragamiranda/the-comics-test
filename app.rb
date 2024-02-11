6e9ba521ede64a6a51de3681ebbb5498d5af2116
Pedro Braga Miranda <pbragamiranda@pm.me>
2024-02-11 20:12:15 +0100

    marvel api service + basic view

--- app.rb
+++ app.rb
@@ -4,4 +4,8 @@ require_relative 'config/application'
 require 'sinatra'
 
 get '/' do
+  marvel_api_service = MarvelApiService.new
+  @story = marvel_api_service.fetch_random_story_by_character(MarvelApiService::SUNSPOT_CHARACTER_ID)
+  @characters = marvel_api_service.fetch_story_characters(@story)
+  erb :home
 end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# Require gems
+require 'json'
+require 'open-uri'
+require 'digest/md5'
+require 'dotenv/load'
+
 # Load all models
 Dir["#{__dir__}/../models/*.rb"].sort.each { |file| require file }
+# Load all services
+Dir["#{__dir__}/../services/*.rb"].sort.each { |file| require file }
